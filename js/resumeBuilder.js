b202b463eb01c0ba783fad0c83264f7199c16c48
Adriana Molina <moladriana@gmail.com>
2015-09-23 10:42:20 +0100

    Append and prepend examples

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,15 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+	// $("#main").append("Adriana Molina");
+	// var sentence = "I am Adriana and I am AWESOME";
+	// var newSentence = sentence.replace ("AWESOME", "FUN");
+
+	// console.log (sentence);
+	// console.log (newSentence);
+
+	var formattedName = HTMLheaderName.replace ("%data%", "Adriana Molina");
+	var formattedRole = HTMLheaderRole.replace ("%data%", "Frontend");
+
+	$("#header").append(formattedName);
+	$("#header").append(formattedRole);
+
+
+
