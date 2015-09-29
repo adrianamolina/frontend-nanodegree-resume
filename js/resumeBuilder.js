3a262239d309daaa7400c5cbac874d06708f13a9
Adriana Molina <moladriana@gmail.com>
2015-09-29 13:53:18 +0100

    Projects object added

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,31 +3,31 @@ var bio = {
 	"name" : "Adriana" ,
 	"role" : "Frontend" ,
 	"contactInfo" : "adriana@blabla.com"
-}
+};
 
 
 var education = {
-		"schools" : [
-			{
-				"school" : "Liceo",
-				"city" : "Marte",
-				"degree" : "Adulthood",
-				"years" : 12
-			},
-			{
-				"school" : "Complutense",
-				"city" : "Madrid",
-				"degree" : "Audiovisual",
-				"years" : 5
-			},
-			{
-				"school" : "Udacity",
-				"city" : "internet",
-				"degree" : "frontend",
-				"years" : 1
-			}
-		]
-	};
+	"schools" : [
+		{
+			"school" : "Liceo",
+			"city" : "Marte",
+			"degree" : "Adulthood",
+			"years" : 12
+		},
+		{
+			"school" : "Complutense",
+			"city" : "Madrid",
+			"degree" : "Audiovisual",
+			"years" : 5
+		},
+		{
+			"school" : "Udacity",
+			"city" : "internet",
+			"degree" : "frontend",
+			"years" : 1
+		}
+	]
+};
 
 var work = {
 	"experience" : [
@@ -50,8 +50,28 @@ var work = {
 			"years" : 2
 		}
 	]
-}
+};
 
 var project = {
-
-}
+	"DIY" : [
+		{	
+			"objects" : "bags",
+			"materials" : ["fabrics", "threads", "haberdashery"],
+			"tools" : ["machine", "scissors", "pins"]
+		},
+		{
+			"objects" : "bracelet",
+			"materials" : ["waxed cotton", "beads", "buttons"],
+			"tools" : ["table", "lighter"]
+		},
+		{
+			"objects" : "sweater",
+			"materials" : ["yarn", "cotton"],
+			"tools" : ["needles", "cables", "marker"]
+		}
+	],
+	"Frontend" : {
+		"skills" : ["html", "javascript", "css"],
+		"exercises" : "resume"
+	}
+};
